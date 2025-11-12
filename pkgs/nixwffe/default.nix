9b0b88c0998f2faa84444944c5d35d81ade4fba6
Yifei Sun <ysun@hey.com>
2025-11-12 12:56:49 +0100

    pkgs/nixwffe: revert fix patching logic
    This reverts commit 24ce19b4c1706f62c177446b00046fa439f9fd89.

--- pkgs/nixwffe/default.nix
+++ pkgs/nixwffe/default.nix
@@ -1,7 +1,7 @@
 { nixVersions }:
 
-nixVersions.latest.override {
-  nix-flake = nixVersions.latest.libs.nix-flake.overrideAttrs (prev: {
-    patches = prev.patches or [ ] ++ [ ./0001-nix-with-full-flake-expr.patch ];
-  });
-}
+nixVersions.latest.overrideAttrs (prev: {
+  patches = prev.patches or [ ] ++ [
+    ./0001-nix-with-full-flake-expr.patch
+  ];
+})
