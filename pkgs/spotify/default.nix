feb897d986807656348c014cba19a103c7c26f5c
Yifei Sun <ysun@hey.com>
2025-11-29 00:37:26 +0100

    pkgs/spotify: formatting

--- pkgs/spotify/default.nix
+++ pkgs/spotify/default.nix
@@ -1,14 +1,14 @@
-{
-  stdenv,
-  spotify,
+{ stdenv
+, spotify
 }:
 
 if stdenv.hostPlatform.isDarwin then
-  spotify.overrideAttrs (_: {
+  spotify.overrideAttrs
+  {
     postInstall = ''
       rm -f $out/Applications/Spotify.app/Contents/Resources/Icon.icns
       cp ${./icon.icns} $out/Applications/Spotify.app/Contents/Resources/Icon.icns
     '';
-  })
+  }
 else
   spotify
