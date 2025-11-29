356ef58b340d0ba6127e00069f90b98d221efb30
Yifei Sun <ysun@hey.com>
2025-11-29 15:39:35 +0100

    pkgs/ysun: auto pkg import

--- pkgs/ysun/flake.nix
+++ pkgs/ysun/flake.nix
@@ -3,24 +3,21 @@
     systems = import systems;
 
     perSystem = { lib, pkgs, system, ... }: {
-      _module.args = lib.fix (self: {
-        lib = builtins // parts.lib // nixpkgs.lib;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              ocamlPackages = prev.ocamlPackages.overrideScope (ocamlFinal: ocamlPrev:
-                (with self.lib; genAttrs
-                  (attrNames (readDir ./pkgs))
-                  (name: ocamlFinal.callPackage ./pkgs/${name} { }))
-                //
-                {
-                  dune = ocamlPrev.dune_3;
-                });
-            })
-          ];
-        };
-      });
+      _module.args.pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            ocamlPackages = prev.ocamlPackages.overrideScope (ocamlFinal: ocamlPrev:
+              (with lib; genAttrs
+                (attrNames (builtins.readDir ./pkgs))
+                (name: ocamlFinal.callPackage ./pkgs/${name} { }))
+              //
+              {
+                dune = ocamlPrev.dune_3;
+              });
+          })
+        ];
+      };
 
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
@@ -43,44 +40,13 @@
         ${lib.getExe pkgs.ocamlPackages.dune} fmt
       '';
 
-      packages = lib.fix (self: {
-        default = self.cohttp-eio;
-
-        inherit (pkgs.ocamlPackages)
-          carton
-          carton-git-lwt
-          carton-lwt
-          cohttp
-          cohttp-eio
-          git-kv
-          git-net
-          hilite
-          http
-          liquid_interpreter
-          liquid_ml
-          liquid_parser
-          liquid_std
-          liquid_syntax
-          omd
-          oniguruma
-          plist-xml
-          textmate-language
-          yocaml
-          yocaml_cmarkit
-          yocaml_eio
-          yocaml_git
-          yocaml_jingoo
-          yocaml_liquid
-          yocaml_markdown
-          yocaml_mustache
-          yocaml_omd
-          yocaml_otoml
-          yocaml_runtime
-          yocaml_syndication
-          yocaml_unix
-          yocaml_yaml
-          ;
-      });
+      packages = with lib; fix (self: (
+        (genAttrs
+          (attrNames (builtins.readDir ./pkgs))
+          (name: pkgs.ocamlPackages.${name}))
+        //
+        { default = self.cohttp-eio; }
+      ));
     };
   };
 
