fe81aa719a34fc1bd04b73b8d32fafbaa96b0d91
Yifei Sun <ysun@hey.com>
2025-12-03 22:49:40 +0100

    pkgs/aoc: setup extraction

--- pkgs/aoc/bin/y2025/prelude.v
+++
@@ -1,4 +0,0 @@
-From Corelib Require Extraction.
-
-From aoc.Y2025 Require Import D1P1.
-Separate Extraction D1P1.solve.
---
+++ pkgs/aoc/theories/extraction/prelude.v
@@ -0,0 +1,4 @@
+From Corelib Require Extraction.
+
+From AoC.Y2025 Require Import D1P1.
+Extraction "Y2025_D1_P1.ml" D1P1.solve.
