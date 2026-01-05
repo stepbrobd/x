3e306d1c8d3ea6caadbbc3047ebc5930adc278a7
Yifei Sun <ysun@hey.com>
2026-01-05 13:32:31 +0800

    pkgs/nixvim: (temp) disable issues caused by tree-sitter

--- pkgs/nixvim/treesitter.nix
+++ pkgs/nixvim/treesitter.nix
@@ -4,13 +4,13 @@
   plugins = {
     treesitter.enable = true;
     treesitter.folding.enable = true;
-    treesitter-context.enable = true;
-    treesitter-refactor.enable = true;
-    treesitter-textobjects.enable = true;
+    # treesitter-context.enable = true;
+    # treesitter-refactor.enable = true;
+    # treesitter-textobjects.enable = true;
   };
 
   extraPlugins = with pkgs.vimPlugins; [
-    nvim-treesitter-sexp
+    # nvim-treesitter-sexp
     pkgs.treesitter-coq-grammar
   ];
 
